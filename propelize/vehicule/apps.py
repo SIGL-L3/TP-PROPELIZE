0090844248c5eef9fd4bfb9e3582e3294f4e7d84
Spakaime <odin62139@gmail.com>
2025-04-12 06:25:30 +0100

    [UPDT] : Update code

--- propelize/vehicule/apps.py
+++ propelize/vehicule/apps.py
@@ -5,6 +5,6 @@ class VehiculeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehicule'
     
-    # def ready(self):
-    #     from .views import load_initial_data
-    #     load_initial_data()
+    def ready(self):
+        from .views import load_initial_data
+        load_initial_data()
