dc41c1df9d69b68ba39fe986b201a7b60586a385
olive <olive@gmail.com>
2025-04-21 19:16:21 +0100

    Ajout de la fonction delete_vehicule qui permet de supprimer un vehicule a partir de son ID

--- propelize/vehicule/views.py
+++ propelize/vehicule/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 #mes modifs
+
+from vehicule.models import Vehicule
+
+def delete_vehicule(vehicule_id):
+    try:
+        vehicule = Vehicule.objects.get(id=vehicule_id)
+        vehicule.delete()
+        print(f"Véhicule avec l'ID '{vehicule_id}' supprimé avec succès.")
+    except Vehicule.DoesNotExist:
+        print(f"Véhicule avec l'ID '{vehicule_id}' introuvable.")
+    except Exception as e:
+        print(f"Erreur lors de la suppression : {str(e)}")
