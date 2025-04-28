09a456a443b978528c7db9c4871ad17120c3bea0
TracyMikam <mikamtracy@gmail.com>
2025-04-28 11:35:12 +0100

    Tracy (#2)
    * init model
    
    * add seeders
    
    * [ADD]: AJout de l'api pour creer un vehicule et mise a jour du requirement
    
    * [UPDT] : Update code
    
    * [UPDT] : Update code
    
    * [UPDT]: The last update
    
    * Mise a jour du Readme
    
    * readme
    
    * MIse a jour du readme
    
    * readme
    
    * capture et readme
    
    * mise a jour du requirement
    
    * CREATION DE VEHICULE
    
    * creation de vehicule
    
    ---------
    
    Co-authored-by: emilekmd <cabtel558@gmail.com>
    Co-authored-by: Spakaime <odin62139@gmail.com>
    Co-authored-by: Emile_kmd <132402396+emilekmd@users.noreply.github.com>

--- propelize/vehicule/views.py
+++ propelize/vehicule/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from vehicule.models import Vehicule
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+
+class VehiculeView(APIView):
+    def post(self,request):
+        data = json.loads(request.body)
+        if request.method == "POST":
+            required_fields = ["registration_number", "make", "model", "year", "rentalprice"]
+            for field in required_fields:
+                if field not in data:
+                    return JsonResponse({"error": f"Le champ {field} est requis."}, status=400)
+
+            vehicule = Vehicule.objects.create(
+                registration_number=data["registration_number"],
+                make=data["make"],
+                model=data["model"],
+                year=data["year"],
+                rentalprice=data["rentalprice"]
+            )
+            print(data)
+        print('test')
+        print(vehicule)
+        return Response({"Success Create vehicule"})
