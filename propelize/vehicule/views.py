a623cc9e71552b6f4f9a5a0514e4561a624b963c
emilekmd <cabtel558@gmail.com>
2025-05-01 15:31:50 +0100

    update create api with serializer

--- propelize/vehicule/views.py
+++ propelize/vehicule/views.py
@@ -1,28 +1,14 @@
-import json
-from django.http import JsonResponse
 from rest_framework.views import APIView
-from vehicule.models import Vehicule
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-
+from .serializers import VehiculeSerializer
+from rest_framework import status
 
 class VehiculeView(APIView):
-    def post(self,request):
-        data = json.loads(request.body)
-        if request.method == "POST":
-            required_fields = ["registration_number", "make", "model", "year", "rentalprice"]
-            for field in required_fields:
-                if field not in data:
-                    return JsonResponse({"error": f"Le champ {field} est requis."}, status=400)
+    def post(self, request):
+        serializer = VehiculeSerializer(data=request.data)
 
-            vehicule = Vehicule.objects.create(
-                registration_number=data["registration_number"],
-                make=data["make"],
-                model=data["model"],
-                year=data["year"],
-                rentalprice=data["rentalprice"]
-            )
-            print(data)
-        print('test')
-        print(vehicule)
-        return Response({"Success Create vehicule"})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
