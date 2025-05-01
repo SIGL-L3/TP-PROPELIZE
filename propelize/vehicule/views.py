a8052c30e4d625a57c2d5dca479f0800320fd505
TracyMikam <mikamtracy@gmail.com>
2025-05-01 20:41:28 +0100

    blocage de la création de plusieurs vehivules identiques(avec le meme register_number)

--- propelize/vehicule/views.py
+++ propelize/vehicule/views.py
@@ -14,15 +14,28 @@ class VehiculeView(APIView):
             for field in required_fields:
                 if field not in data:
                     return JsonResponse({"error": f"Le champ {field} est requis."}, status=400)
-
-            vehicule = Vehicule.objects.create(
-                registration_number=data["registration_number"],
-                make=data["make"],
-                model=data["model"],
-                year=data["year"],
-                rentalprice=data["rentalprice"]
-            )
-            print(data)
+            
+            vehicules = Vehicule.objects.all()
+            vehicule_existe = False
+            for vehicule in vehicules:
+                if data["registration_number"] == vehicule.registration_number:
+                    vehicule_existe = True
+                    break  # Sortir de la boucle dès qu'on trouve une correspondance
+            
+            if vehicule_existe:
+                print("vehicule existant")
+                return JsonResponse({"error": "Impossible de creer un vehicule deja existant !!!"}, status=400)
+            else:
+                new_vehicule = Vehicule.objects.create(
+                    registration_number=data["registration_number"],
+                    make=data["make"],
+                    model=data["model"],
+                    year=data["year"],
+                    rentalprice=data["rentalprice"]
+                )
+                print(new_vehicule)
+                print(data)
+                return Response({"Success": "Success Create vehicule !!"})
         print('test')
-        print(vehicule)
-        return Response({"Success Create vehicule"})
+        
+        
