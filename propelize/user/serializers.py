05e0e11f9a42750ec4ccf04989d5115227a543ff
emilekmd <cabtel558@gmail.com>
2025-05-17 20:18:02 +0100

    add model user and starting test of vehicule

--- propelize/user/serializers.py
+++ propelize/user/serializers.py
@@ -4,4 +4,15 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = '__all__'
+        fields = ['name','password']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user=User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
