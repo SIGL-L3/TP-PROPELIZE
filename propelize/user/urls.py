d9caa9fd428c061457a9fe575b0b93f2d207a28f
Emile_kmd <132402396+emilekmd@users.noreply.github.com>
2025-05-18 19:53:49 +0100

    Update urls.py
    fix root syntax error

--- propelize/user/urls.py
+++ propelize/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import  path
 from .views import UserView
 
 urlpatterns = [
-    path('user/create',UserView.as_view(),name='create-user'),
-    path('user/update',UserView.as_view(),name='update-user'),
+    path('user/create/',UserView.as_view(),name='create-user'),
+    path('user/update/',UserView.as_view(),name='update-user'),
 ]
